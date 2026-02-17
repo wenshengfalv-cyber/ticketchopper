4ba27d0c2c1b57988ba04f2da2ffa13c8d5bfbcf
09liweis <weisen.li@hotmail.com>
2026-02-17 09:38:52 -0500

    refactor about us lang

--- pages/[lang]/about-us.vue
+++ pages/[lang]/about-us.vue
@@ -10,7 +10,7 @@ definePageMeta({
 const route = useRoute()
 const { locale, t } = useI18n()
 
-const lang = (route.params.lang === 'en') ? 'en' : 'zh'
+const lang = route.params.lang || 'zh'
 locale.value = lang
 const currentLang = lang
 
